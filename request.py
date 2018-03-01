92acb0bec224523fcdc1fc95bea5de9e04e77617
Ilya Bazhenov <31971067+bazhil@users.noreply.github.com>
2018-03-01 19:58:20 +0300

    Add files via upload

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
-def translate_it(text):
+def translate_it(source, result, source_lang, result_lang='ru'):
     """
     YANDEX translation plugin
     docs: https://tech.yandex.ru/translate/doc/dg/reference/translate-docpage/
@@ -14,53 +14,27 @@ def translate_it(text):
     :param text: <str> text for translation.
     :return: <str> translated text.
     """
-    url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-    key = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
-    text=open('DE.txt', 'r')
-    text_de = open('text_de.txt', 'w')
 
-    params = {
-        'key': key,
-        'lang': 'de-ru',
-        'text': text,
-    }
+    with open(source, encoding='utf-8') as file:
 
-    response = requests.get(url, params=params).json()
-    print(' '.join(response.get('text', [])))
+        url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+        key = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
 
-    text_de.write(' '.join(response.get('text', [])))
-    text_de.close()
-    text.close()
+        lang = source_lang + '-' + result_lang
 
-    text = open('ES.txt', 'r')
-    text_es = open('text_es.txt', 'w')
-    params = {
-        'key': key,
-        'lang': 'es-ru',
-        'text': text,
-    }
+        params = {
+            'key': key,
+            'lang': lang,
+            'text': file,
+        }
 
-    response = requests.get(url, params=params).json()
-    print(' '.join(response.get('text', [])))
+        response = requests.get(url, params=params).json()
+        text = (' '.join(response.get('text', []))).replace('\n', '')
+        result_text = open(result, 'w')
+        result_text.write(text)
 
-    text_es.write(' '.join(response.get('text', [])))
-    text_es.close()
-    text.close()
+translate_it('ES.txt', 'text_ES.txt', 'es', 'ru')
+translate_it('FR.txt', 'text_FR.txt', 'fr', 'ru')
+translate_it('DE.txt', 'text_DE.txt', 'de', 'ru')
 
-    text = open('FR.txt', 'r')
-    text_fr = open('text_fr.txt', 'w')
-    params = {
-        'key': key,
-        'lang': 'fr-ru',
-        'text': text,
-    }
 
-    response = requests.get(url, params=params).json()
-    print(' '.join(response.get('text', [])))
-
-    text_fr.write(' '.join(response.get('text', [])))
-    text_fr.close()
-    text.close()
-
-a = translate_it('Привет')
-print(a)
